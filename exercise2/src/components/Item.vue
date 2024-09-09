06e86c4ba3a94d023fbccbf5beae6fdca2cbbba2
hayapy88 <payagram01@gmail.com>
2024-09-10 05:16:54 +1000

    Apply styling, responsive design, and improve UX

--- exercise2/src/components/Item.vue
+++ exercise2/src/components/Item.vue
@@ -1,16 +1,38 @@
 <template>
-  <div class="item">
-    <p class="item__title">{{ title }}</p>
-    <div class="item__content" v-html="content"></div>
+  <div class="item" :class="{ 'is-active': isActive }">
+    <h2 class="item__header" @click="$emit('toggle')">
+      <component :is="getIcon" class="item__icon size-5 text-black" />
+      {{ title }}
+      <component
+        :is="isActive ? ChevronUpIcon : ChevronDownIcon"
+        class="item__chevron size-4"
+      />
+    </h2>
+    <div class="item__content" v-if="isActive" v-html="content"></div>
   </div>
 </template>
 
 <script>
+import * as SolidIcons from "@heroicons/vue/24/solid";
+
 export default {
   name: "Item",
   props: {
     title: { type: String, required: true },
     content: { type: String, required: true },
+    icon: { type: String, required: true },
+    isActive: { type: Boolean, default: false },
+  },
+  computed: {
+    getIcon() {
+      return SolidIcons[this.icon];
+    },
+    ChevronUpIcon() {
+      return SolidIcons.ChevronUpIcon;
+    },
+    ChevronDownIcon() {
+      return SolidIcons.ChevronDownIcon;
+    },
   },
 };
 </script>
--- exercise2/src/components/Items.vue
+++ exercise2/src/components/Items.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="items">
-    <Item
-      v-for="(item, index) in items"
-      :key="index"
-      :title="item.title"
-      :content="item.content"
-    />
+    <h1 class="items__title">Items list</h1>
+    <div class="items__list">
+      <Item
+        v-for="(item, index) in items"
+        :key="index"
+        :title="item.title"
+        :content="item.content"
+        :icon="item.icon"
+        :isActive="index === activeIndex"
+        @toggle="toggleItem(index)"
+      />
+    </div>
   </div>
 </template>
 
@@ -17,5 +23,16 @@ export default {
     Item,
   },
   props: { items: { type: Array, required: true } },
+  data() {
+    return {
+      activeIndex: 0,
+    };
+  },
+  methods: {
+    toggleItem(index) {
+      console.log("Toggling item at index:", index);
+      this.activeIndex = this.activeIndex === index ? -1 : index;
+    },
+  },
 };
 </script>
